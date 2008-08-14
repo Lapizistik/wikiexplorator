a3790e25ea5f5d8370bd4c1205e1d1579202ca48
wegener <wegener@17182e90-d3dd-df11-a988-0025b3ada58c>
2008-08-14 09:32:47 +0000

    git-svn-id: file:///tmp/g/svn/mediawikiparser@245 17182e90-d3dd-df11-a988-0025b3ada58c

---
+++ Visualizer/src/visualizer/display/ItemComparator.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package visualizer.display;
+
+import java.util.Comparator;
+
+import prefuse.visual.VisualItem;
+
+/**
+ * @author rene
+ *
+ */
+public class ItemComparator implements Comparator<VisualItem>
+{
+	public int compare(VisualItem arg0, VisualItem arg1) 
+	{
+		double[] values1 = ((double[])arg0.get("value"));
+		double[] values2 = ((double[])arg1.get("value"));
+		double val1 = 0, val2 = 0; 
+		for (int i = 0; i < values1.length; i++)
+		{
+			val1 += values1[i];
+			val2 += values2[i];
+		}
+		if (val1 < val2)
+			return -1;
+		else if (val1 > val2)
+			return 1;
+		else
+			return 0;
+	}
+
+}
